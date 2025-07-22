4b03dfa23766a203ad872c5062e19b7b0435212e
unknown <bieldanil240@gamil.com>
2025-07-22 16:31:35 +0200

    .

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,16 @@
 const modal = document.querySelector("#myModal");
 const btn = document.querySelector(".hero-info-btn");
 const span = document.querySelector(".close");
+const button = document.querySelector(".modal-submit-btn");
 
 // Открыть модал при клике на кнопку
 btn.onclick = function () {
   modal.style.display = "block";
+};
+button.onclick = function () {
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 300);
 };
 
 // Закрыть модал при клике на крестик
