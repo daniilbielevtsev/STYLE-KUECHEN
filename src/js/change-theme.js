d40e0569346964d2991001b816c6c74c6db45611
unknown <bieldanil240@gamil.com>
2025-09-09 16:52:03 +0200

    .

---
+++ src/js/change-theme.js
@@ -0,0 +1,47 @@
+const darkTheme = {
+  "--text-color-header": "#000000be",
+  "--header-button-color": "#fff",
+  "--main-titel-color": "#e9dfd6ff",
+  "--main-text-color": "rgba(255, 255, 255, 0.85)",
+  "--body-bg-color": "#2D261F",
+  "--titel-header": "#d6d0c8ff",
+};
+
+const lightTheme = {
+  "--text-color-header": "#ffffff",
+  "--header-button-color": "#000",
+  "--main-titel-color": "#453030",
+  "--main-subtitel-color": "#957d62",
+  "--main-text-color": "rgba(0, 0, 0, 0.75)",
+  "--footer-bg-color": "#372626",
+  "--body-bg-color": "#f8f5f2",
+  "--titel-header": "#ffffffff",
+};
+
+const toggleButton = document.querySelector("#toggleTheme");
+let dark = false;
+
+toggleButton.addEventListener("click", () => {
+  const theme = dark ? lightTheme : darkTheme;
+  for (const key in theme) {
+    document.documentElement.style.setProperty(key, theme[key]);
+  }
+  dark = !dark;
+});
+// ======================
+const btn = document.querySelector("#toggleTheme");
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("light-mode")) {
+    btn.classList.remove("light-mode");
+    btn.classList.add("dark-mode");
+    document.body.classList.add("dark");
+  } else {
+    btn.classList.remove("dark-mode");
+    btn.classList.add("light-mode");
+    document.body.classList.remove("dark");
+  }
+});
+
+// начальное состояние
+btn.classList.add("light-mode");
